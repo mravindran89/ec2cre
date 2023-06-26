e5129b6d5991fc1928c9cf1a3a4aac32f359e704
ravi <ravi>
2023-06-26 16:16:08 +0530

    fourth

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
-resource "aws_instance" "ec2_instance"
-    {
+resource "aws_instance" "ec2_instance" {
     ami 	= var.ami_id
     count 	= var.number_of_instances
     subnet_id 	= var.subnet_id
--- var.tf
+++ var.tf
@@ -1,30 +1,38 @@
 variable "instance_name" {
         description = "Name of the instance to be created"
+	type    = string
         default = "awsbuilder-demo"
 }
 
 variable "instance_type" {
+	type    = string
         default = "t2.micro"
 }
 
 variable "subnet_id" {
-        description = "The VPC subnet the instance(s) will be created in"
+	description = "The VPC subnet the instance(s) will be created in"
+	type    = string
         default = "subnet-059e3fae13d8bb802"
+
 }
 
 variable "ami_id" {
         description = "The AMI to use"
+	type    = string
         default = "ami-057752b3f1d6c4d6c"
 }
 
 variable "number_of_instances" {
         description = "number of instances to be created"
+	type    = string
         default = 1
 }
 
-
 variable "ami_key_pair_name" {
+	description = "Name of the SSH key"
+	type    = string
         default = "NEWSERVERS.pem"
+	
 
   }
 
