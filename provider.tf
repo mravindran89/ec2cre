4d2cb90fdc1642a812158c4ef83ca7ea9d328680
ravi <ravi>
2023-06-26 16:53:48 +0530

    sixth

--- provider.tf
+++ provider.tf
@@ -1,9 +1,7 @@
 terraform {
-    required_providers
-        {
-    aws =
-       {
-        source = "hasicorp/aws"
+    required_providers {
+    aws = {
+        source = "hashicorp/aws"
         version = "~> 4.0"      
       }
     }
